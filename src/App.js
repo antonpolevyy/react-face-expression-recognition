c114bf704eee845b4b4fe7c875bf0a047d80deaa
Anton P <polevyy.anton@gmail.com>
2019-11-01 02:57:25 +1100

    Home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import './App.css';
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router history={ createBrowserHistory({ basename: process.env.PUBLIC_URL }) }>
+          <Route exact path='/' component={ Home } />
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
